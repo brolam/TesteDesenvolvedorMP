59058e46fe491df344633e3cf52d0224edf0c1d2
Breno Marques <breno@brolam.com.br>
2017-06-24 17:30:43 -0300

    feat: Concluído Ciclo TDD - Avaliação do Conhecimento. this closed #4

--- webapps/candidates/models.py
+++ webapps/candidates/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.core.exceptions import ValidationError
+
+SKILL_TECHNOLOGY_RATING_CHOICES = [(0,0), (1,1), (2,2), (3,3), (4,4) ,(5,5) ,(6,6), (7,7) , (8,8), (9,9), (10,10)]
+
+def validate_skill_tec_rating(value):
+		if ( value, value ) in SKILL_TECHNOLOGY_RATING_CHOICES :  return
+		raise ValidationError(_('This skill rating should be between 1 and 10.'))
+
+class SkillTecRatingField(models.IntegerField):
+		def __init__(self, *args, **kwargs):
+			kwargs['choices'] = SKILL_TECHNOLOGY_RATING_CHOICES
+			kwargs['blank']=True 
+			kwargs['default']=0
+			kwargs['validators']=[validate_skill_tec_rating]
+			super(models.IntegerField, self).__init__(*args, **kwargs)
 
 class Evaluation(models.Model):
  	name = models.CharField(_("name"), max_length=60, null=False, blank=False)
  	email = models.EmailField(_("email"), max_length=60, null=False, blank=False)
+ 	skill_tec_html_rating = SkillTecRatingField( _("HTML") )
+ 	skill_tec_css_rating = SkillTecRatingField( _("CSS"))
+	skill_tec_javascript_rating = SkillTecRatingField( _("Javascript") )
+	skill_tec_python_rating = SkillTecRatingField( _("Python") )
+	skill_tec_django_rating = SkillTecRatingField( _("Django") )
+	skill_tec_ios_rating = SkillTecRatingField( _("Development iOS") )
+	skill_tec_android_rating = SkillTecRatingField( _("Development Android") )
 
  	def __unicode__(self):
  		return u'%s / %s' % (self.name, self.email)
--- webapps/candidates/tests.py
+++ webapps/candidates/tests.py
@@ -1,36 +1,57 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from candidates.models import Evaluation 
 from django.core.exceptions import ValidationError
 
 class CandidateEvaluationModelTest(TestCase):
+    def setUp(self):
+        self.evaluation = Evaluation(name="CandidateEvaluation", email="candidateEvaluation@mp.com")
 
     def test_create_one_evaluation(self):
-        evaluation = Evaluation(name="CandidateEvaluation", email="candidateEvaluation@mp.com")
-        evaluation.save()
+        self.evaluation.save()
         candidateEvaluation = Evaluation.objects.get(name="CandidateEvaluation")
         self.assertEqual(candidateEvaluation.name, 'CandidateEvaluation')
         self.assertEqual(candidateEvaluation.email, 'candidateEvaluation@mp.com')
 
-    def test_evaluation_name_is_required(self):
-        evaluation = Evaluation(email="candidateEvaluation@mp.com")
+    def test_name_is_required(self):
+        self.evaluation.name = "";
         try:
-            evaluation.clean_fields()
+            self.evaluation.clean_fields()
         except ValidationError:
             return
         self.fail('Field Name must be required!')
 
-    def test_evaluation_email_is_required(self):
-        evaluation = Evaluation(name="CandidateEvaluation")
+    def test_email_is_required(self):
+        self.evaluation.email = "";
         try:
-            evaluation.clean_fields()
+            self.evaluation.clean_fields()
         except ValidationError:
             return
         self.fail('Field Email must be required!')
-   
+
+    def check_one_field_skill_tec_rating_negative(self, skill_tec_field_name):
+        setattr(self.evaluation, skill_tec_field_name, -1)
+        try:
+            self.evaluation.clean_fields()
+            setattr(self.evaluation, skill_tec_field_name, 0)
+        except ValidationError:
+            return
+        self.fail('Field "{p[field]}" can not be Negative!'.format(p={'field': skill_tec_field_name }))
+
+    def check_one_field_skill_tec_rating_greater_than_ten(self, skill_tec_field_name):
+        setattr(self.evaluation, skill_tec_field_name, 11)
+        try:
+            self.evaluation.clean_fields()
+            setattr(self.evaluation, skill_tec_field_name, 0)
+        except ValidationError:
+            return
+        self.fail('Field "{p[field]}" can not be greater than ten!'.format(p={'field': skill_tec_field_name }))
+
+    def test_all_fields_skill_tec_rating_negative(self):
+        for skill_tec_field in self.evaluation._meta.get_fields():
+            if ( skill_tec_field.name.startswith( 'skill_tec_' ) ):
+                self.check_one_field_skill_tec_rating_negative(skill_tec_field.name)
+
+    def test_all_field_skill_tec_rating_greater_than_ten(self):
+         for skill_tec_field in self.evaluation._meta.get_fields():
+            if ( skill_tec_field.name.startswith( 'skill_tec_' ) ):
+                self.check_one_field_skill_tec_rating_greater_than_ten(skill_tec_field.name)
