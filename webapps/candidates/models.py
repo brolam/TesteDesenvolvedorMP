8128e8950c934985bd6340a58eadf0d05cc45775
Breno Marques <breno@brolam.com.br>
2017-06-24 20:45:19 -0300

    feat: Concluído Ciclo TDD - Regras para Identificar a Área do Desenvolvedor. This closed #5

--- webapps/candidates/models.py
+++ webapps/candidates/models.py
@@ -4,16 +4,16 @@ from django.core.exceptions import ValidationError
 
 SKILL_TECHNOLOGY_RATING_CHOICES = [(0,0), (1,1), (2,2), (3,3), (4,4) ,(5,5) ,(6,6), (7,7) , (8,8), (9,9), (10,10)]
 
-def validate_skill_tec_rating(value):
-		if ( value, value ) in SKILL_TECHNOLOGY_RATING_CHOICES :  return
-		raise ValidationError(_('This skill rating should be between 1 and 10.'))
-
 class SkillTecRatingField(models.IntegerField):
+		def validate_skill_tec_rating(self, value):
+			if ( value, value ) in SKILL_TECHNOLOGY_RATING_CHOICES :  return
+			raise ValidationError(_('This skill rating should be between 1 and 10.'))
+
 		def __init__(self, *args, **kwargs):
 			kwargs['choices'] = SKILL_TECHNOLOGY_RATING_CHOICES
 			kwargs['blank']=True 
 			kwargs['default']=0
-			kwargs['validators']=[validate_skill_tec_rating]
+			kwargs['validators']=[self.validate_skill_tec_rating]
 			super(models.IntegerField, self).__init__(*args, **kwargs)
 
 class Evaluation(models.Model):
@@ -26,6 +26,32 @@ class Evaluation(models.Model):
 	skill_tec_django_rating = SkillTecRatingField( _("Django") )
 	skill_tec_ios_rating = SkillTecRatingField( _("Development iOS") )
 	skill_tec_android_rating = SkillTecRatingField( _("Development Android") )
+
+	def is_development_front_end(self):
+		return (
+			7 <= self.skill_tec_html_rating <= 10 and 
+			7 <= self.skill_tec_css_rating <= 10 and
+			7 <= self.skill_tec_javascript_rating <= 10
+		)
+
+	def is_development_back_end(self):
+		return (
+			7 <= self.skill_tec_python_rating <= 10 and 
+			7 <= self.skill_tec_django_rating <= 10
+		)
+
+	def is_development_mobile(self):
+		return (
+			7 <= self.skill_tec_ios_rating <= 10 and 
+			7 <= self.skill_tec_android_rating <= 10
+		)
+
+	def is_development_generic(self):
+		return (
+			not self.is_development_front_end() and 
+			not self.is_development_back_end() and 
+			not self.is_development_mobile() 
+		)
 
  	def __unicode__(self):
  		return u'%s / %s' % (self.name, self.email)
--- webapps/candidates/tests.py
+++ webapps/candidates/tests.py
@@ -55,3 +55,75 @@ class CandidateEvaluationModelTest(TestCase):
          for skill_tec_field in self.evaluation._meta.get_fields():
             if ( skill_tec_field.name.startswith( 'skill_tec_' ) ):
                 self.check_one_field_skill_tec_rating_greater_than_ten(skill_tec_field.name)
+
+    def test_is_or_not_development_front_end(self):
+        for rating in range(7, 11):
+            msg_rating = ' On rating: %s.' %(rating)
+            self.evaluation.skill_tec_html_rating = rating
+            self.evaluation.skill_tec_css_rating  = rating
+            self.evaluation.skill_tec_javascript_rating = rating
+            self.assertEqual(self.evaluation.is_development_front_end(), True, msg_rating)
+            self.evaluation.skill_tec_javascript_rating = 6
+            self.assertEqual(self.evaluation.is_development_front_end(), False, msg_rating)
+            self.evaluation.skill_tec_javascript_rating = rating
+            self.assertEqual(self.evaluation.is_development_front_end(), True, msg_rating)
+            self.evaluation.skill_tec_css_rating  = 6
+            self.assertEqual(self.evaluation.is_development_front_end(), False, msg_rating)
+            self.evaluation.skill_tec_css_rating  = rating
+            self.assertEqual(self.evaluation.is_development_front_end(), True, msg_rating)
+            self.evaluation.skill_tec_html_rating = 6
+            self.assertEqual(self.evaluation.is_development_front_end(), False, msg_rating)
+            self.evaluation.skill_tec_html_rating = rating
+            self.assertEqual(self.evaluation.is_development_front_end(), True, msg_rating)
+
+    def test_is_or_not_development_back_end(self):
+        for rating in range(7, 11):
+            msg_rating = ' On rating: %s.' %(rating)
+            self.evaluation.skill_tec_python_rating = rating
+            self.evaluation.skill_tec_django_rating  = rating
+            self.assertEqual(self.evaluation.is_development_back_end(), True)
+            self.evaluation.skill_tec_django_rating  = 6
+            self.assertEqual(self.evaluation.is_development_back_end(), False)
+            self.evaluation.skill_tec_django_rating  = rating
+            self.assertEqual(self.evaluation.is_development_back_end(), True)
+            self.evaluation.skill_tec_python_rating = 6 
+            self.assertEqual(self.evaluation.is_development_back_end(), False)
+            self.evaluation.skill_tec_python_rating = rating
+            self.assertEqual(self.evaluation.is_development_back_end(), True)
+
+    def test_is_or_not_development_mobile(self):
+        for rating in range(7, 11):
+            msg_rating = ' On rating: %s.' %(rating)
+            self.evaluation.skill_tec_ios_rating = rating
+            self.evaluation.skill_tec_android_rating  = rating
+            self.assertEqual(self.evaluation.is_development_mobile(), True, msg_rating)
+            self.evaluation.skill_tec_android_rating  = 6
+            self.assertEqual(self.evaluation.is_development_mobile(), False, msg_rating)
+            self.evaluation.skill_tec_android_rating = rating
+            self.assertEqual(self.evaluation.is_development_mobile(), True, msg_rating)
+            self.evaluation.skill_tec_ios_rating = 6
+            self.assertEqual(self.evaluation.is_development_mobile(), False, msg_rating)
+            self.evaluation.skill_tec_ios_rating = rating
+            self.assertEqual(self.evaluation.is_development_mobile(), True, msg_rating)
+
+    def test_is_or_not_development_generic(self):
+        for rating in range(7, 11):
+            msg_rating = ' On rating: %s.' %(rating)
+            self.evaluation.skill_tec_html_rating = 6 
+            self.evaluation.skill_tec_css_rating  = 6
+            self.evaluation.skill_tec_javascript_rating = 6
+            self.evaluation.skill_tec_python_rating = 6
+            self.evaluation.skill_tec_django_rating  = 6
+            self.evaluation.skill_tec_ios_rating = 6 
+            self.evaluation.skill_tec_android_rating  = 6
+            self.assertEqual(self.evaluation.is_development_generic(), True, msg_rating)
+            self.evaluation.skill_tec_html_rating = rating 
+            self.evaluation.skill_tec_css_rating  = rating
+            self.evaluation.skill_tec_javascript_rating = rating
+            self.evaluation.skill_tec_python_rating = rating
+            self.evaluation.skill_tec_django_rating  = rating
+            self.evaluation.skill_tec_ios_rating = rating
+            self.evaluation.skill_tec_android_rating  = rating
+            self.assertEqual(self.evaluation.is_development_generic(), False, msg_rating)
+ 
+
